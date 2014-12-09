537953c7032bf9878aa7efff5962b8b5fb499737
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2014-12-08 18:08:01 -0800

    initial Breadcrumb View

---
+++ public/javascripts/congo/index.js
@@ -0,0 +1,6 @@
+var Congo = {
+  init: function() {
+    var crumbView = new Congo.BreadcrumbView({el: "#breadcrumbs"});
+    crumbView.render();
+  }
+}
---
+++ public/javascripts/congo/nav.js
@@ -0,0 +1,11 @@
+Congo.BreadcrumbView = Backbone.View.extend({
+  render: function() {
+    $(this.el).html('<li><h3><a href="#">DATABASES</a></h3></li>');
+  }, 
+  events: {
+    "click a": "sayHello"
+  },
+  sayHello: function() {
+    console.log("hello");
+  }  
+});
